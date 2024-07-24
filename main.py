213737210307bff6be2cd49661de337e0ada06e8
Sonu Kachhap <166591588+Sonu-77@users.noreply.github.com>
2024-07-24 06:53:46 +0000

    python and js practice

---
+++ main.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+import numpy as np
+
+# arry=[1,2,3,45,6,8,7]
+
+# for i in arry:
+#     print(i)
+
+# num=4
+
+# for i in range(1,num+1):
+#     for j in range(1,i+1):
+#         print('*', end="") 
+
+#     print("")
+
+
+str="SonuKachhap"
+
+lenght=len(str)
+
+print(lenght)
+
+# for i in len(str):
+#     print(i)
+
+typeof=type(str)
+print(typeof)
+
--- test.js
+++ test.js
@@ -1,11 +1,47 @@
-console.log('Sonu')
+// console.log('Sonu')
 
-let num=5;
+// let num=5;
 
-for(i=1;i<=num;i++){
-    let row="";
-    for(j=1;j<=i;j++){
-        row+='*';
-    }
-    console.log(row)
-}
+// for(i=1;i<=num;i++){
+//     let row="";
+//     for(j=1;j<=i;j++){
+//         row+='*';
+//     }
+//     console.log(row)
+// }
+
+// let nowdate= new Date()
+
+// console.log(nowdate.toDateString());
+// console.log(nowdate.toISOString());
+// console.log(nowdate.toJSON());
+// console.log(nowdate.toLocaleDateString());
+// console.log(nowdate.toLocaleString());
+// console.log(nowdate.toLocaleString("default", {
+//     weekday: "long",
+//     year: "numeric",
+//     month: "long",
+//     day: "numeric",
+//     hour: "2-digit",
+//     minute: "2-digit",
+//     second: "2-digit",
+//     timeZoneName: "short"
+// }));
+// console.log(nowdate.toLocaleTimeString());
+// console.log(nowdate.toString());
+// console.log(nowdate.toTimeString());
+// console.log(nowdate.getTimezoneOffset());
+// console.log(nowdate.toUTCString());
+
+
+let anyDate= new Date("2-02-2009")
+
+console.log(anyDate.toLocaleDateString("default",{
+    weekday:"narrow",
+    year:"2-digit",
+    month:"long"
+}));
+
+
+
+
