0f056a2b1c0b739d6df28a17ae8129cf6f891d16
Sonu Kachhap <166591588+Sonu-77@users.noreply.github.com>
2024-07-26 11:02:48 +0000

    practice loops

--- test.js
+++ test.js
@@ -144,8 +144,104 @@ obj1.marks=80
 
 // this practice
 
-console.log(this);
+// console.log(this);
 
-(function thiswhat(){
-    console.log(this)
-})();
+// (function thiswhat(){
+//     console.log(this)
+// })();
+
+
+                                                                            // Filters
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  const filtergenre= books.filter( (book)=>{
+    return book.genre==="History";
+  })
+
+//   console.log(filtergenre);
+
+  const publishafterf= books.filter( (book)=>{
+    return book.publish>=2000
+  })
+
+//   console.log(publishafterf)
+
+
+
+
+                                                // Foreach  
+
+let publishafter =[]
+
+books.forEach( (book)=>{
+    if (book.publish>2000) {
+        return publishafter.push(book)
+    }
+})
+
+// console.log(publishafter);
+
+
+
+
+  let filterGenreHistory=[];
+
+  books.forEach( function(book){
+    if (book.genre==="History") {
+        return filterGenreHistory.push(book)
+        
+    }
+  })
+
+//   console.log(filterGenreHistory);
+
+  const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+  myNums.forEach( (num)=>{
+    // console.log(num)
+  })
+
+
+
+  myNums.forEach( (num)=>{
+    //  console.log(num+100);
+  })
+
+//   REduce
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: "12999"
+    },
+]
+
+const totalcartprice= shoppingCart.reduce( (result,currprice)=>{
+    return result+Number(currprice.price)
+},0)
+
+console.log(totalcartprice);
