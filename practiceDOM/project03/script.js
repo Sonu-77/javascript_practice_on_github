78937fb8ad114e9cc21bec6ee3e1ca7454c78c30
Sonu Kachhap <166591588+Sonu-77@users.noreply.github.com>
2024-07-30 12:38:15 +0000

    Project 3 Completed

--- practiceDOM/project03/script.js
+++ practiceDOM/project03/script.js
@@ -1,50 +1,95 @@
-const button = document.querySelectorAll(".botton-box")
-
-let playerO=true
-
-const winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
+const form = document.querySelector("form")
 
 
-button.forEach( (box)=>{
-    box.addEventListener( "click",(e)=>{
-        if (playerO) {
-            box.innerHTML="O"
-            playerO=false
-        }else{
-            box.innerHTML="X"
-            playerO=true
-        }
-        box.disabled=true
-        checkwinner()
-    })
-    
-})
 
-function checkwinner(){
-    for (const index of winningConditions) {
-        let positiontext0=button[index[0]].innerText
-        let positiontext1=button[index[1]].innerText
-        let positiontext2=button[index[2]].innerText
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    const player1name=document.querySelector("#player1").value
+    const player2name=document.querySelector("#player2").value
+    const buttons=document.querySelectorAll(".button-box")
+    const winner=document.querySelector(".declare-winner")
+    const restart=document.querySelector("#restart")
+    const submit =document.querySelector("#submit")
 
-        if (positiontext0 !="" || positiontext1 != "" || positiontext2 !="") {
-            
-            if (positiontext0==positiontext1 && positiontext1==positiontext2) {
-                console.log("winner is declared");
+    submit.disabled=true
+    restart.style.display="block"
+
+
+
+    let playerturnX=true
+
+    console.log(`player1name : ${player1name}`);
+    console.log(`player2name : ${player2name}`);
+
+
+
+
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    buttons.forEach((button)=>{
+        button.addEventListener("click",(e)=>{
+            if (playerturnX) {
+                button.innerHTML="X"
+                playerturnX=false
+            }else{
+                button.innerHTML="O"
+                playerturnX=true
             }
-        }else{
-            console.log("complete the game")
+            checkwinner(player1name,player2name)
+            button.disabled=true
+        })
+    })
+
+    function checkwinner(player1name,player2name){
+        for (const index of winningConditions) {
+            let positionVal0=buttons[index[0]].textContent
+            let positionVal1=buttons[index[1]].textContent
+            let positionVal2=buttons[index[2]].textContent
+
+            // console.log(positionVal0)
+            // console.log(positionVal1)
+            // console.log(positionVal2)
+            
+
+            if (positionVal0 !="" && positionVal1 !="" && positionVal2 != "") {
+                if (positionVal0==positionVal1 && positionVal1==positionVal2) {
+                    if (positionVal0 && positionVal1 && positionVal2 =="X") {
+                        winner.style.display="block"
+                        winner.innerHTML=`Winner of the match is ${player1name} `
+                    }else{
+                        winner.style.display="block"
+                        winner.innerHTML=`Winner of the match is ${player2name} `
+                    }
+                    // console.log(`Winner is declared`)
+                }else{
+                    // console.log(`continue playing`)
+                }
+            }else{
+                // console.log(`complete the game`);
+            }
         }
-        
-       
     }
-}
+
+    restart.addEventListener("click",(e)=>{
+        buttons.forEach((button)=>{
+            button.innerHTML=""
+            playerturnX=true
+            button.disabled=false
+            // restart.style.display="none"
+            winner.style.display="none"
+            submit.disabled=false
+        })
+        
+    })
+
+})
 
